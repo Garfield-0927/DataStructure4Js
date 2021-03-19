8f59ed92a4970b3b8527aabcd7026e0b249c9cc0
Garfield_zhang <929780650@qq.com>
2021-03-19 21:34:14 +0800

    bst

---
+++ 树/BinarySearchTree.js
@@ -0,0 +1,14 @@
+// 二叉搜索树
+class Node{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST{
+  constructor(){
+    this.root = null;
+  }
+}
