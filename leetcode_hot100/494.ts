028bb7a1a559dcc3b788aeec31524fa215f0247b
Garfield-0927 <929780650@qq.com>
2022-02-21 22:28:18 +0800

    15

--- leetcode_hot100/494.ts
+++ leetcode_hot100/494.ts
@@ -1,3 +1,24 @@
+// function findTargetSumWays(nums: number[], target: number): number {
+//   let res = 0;
+//   const len = nums.length;
+//   const bfs = (i: number, target: number) => {
+//     if (i === len) {
+//       if (!target) {
+//         res++;
+//       }
+//       return;
+//     }
+
+//     bfs(i+1, target + nums[i]);
+//     bfs(i+1, target - nums[i]);
+//   };
+
+//   bfs(0, target);
+
+//   return res;
+// }
+
+
 function findTargetSumWays(nums: number[], target: number): number {
   let res = 0;
   const len = nums.length;
@@ -17,6 +38,5 @@ function findTargetSumWays(nums: number[], target: number): number {
 
   return res;
 }
-
 let res = findTargetSumWays([1, 1, 1, 1, 1], 3);
 console.log(res);
