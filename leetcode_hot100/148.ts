cd08b87afb60ce27358436096cbc647e34f299de
Garfield-0927 <929780650@qq.com>
2022-05-25 00:20:25 +0800

    148

--- leetcode_hot100/148.ts
+++ leetcode_hot100/148.ts
@@ -1,4 +1,65 @@
-import { ListNode, testLinkedList } from './common';
+import { ListNode, testLinkedList, traverseLinkedList } from './common';
+
+const findMidNode = (head: ListNode): ListNode => {
+  if (!head.next) {
+    return head;
+  }
+
+  let slow: ListNode | null = head,
+    fast: ListNode | null = head;
+
+  while (fast && fast.next) {
+    slow = slow.next!;
+    fast = fast?.next?.next;
+  }
+  return slow!;
+};
+
+const mergeTwoOrderedList = (
+  node1: ListNode | null,
+  node2: ListNode | null
+) => {
+  const dummyNode = new ListNode(0, null);
+  let prev = dummyNode;
+
+  while (node1 && node2) {
+    if (node1.val >= node2.val) {
+      prev.next = node2;
+      node2 = node2.next;
+    } else {
+      prev.next = node1;
+      node1 = node1.next;
+    }
+    prev = prev.next;
+  }
+  prev.next = node1 ? node1 : node2;
+  return dummyNode.next;
+};
+
+const mergeSort = (node: ListNode): ListNode => {
+  if (!node.next) {
+    return node;
+  }
+
+  if (!node.next.next) {
+    if (node.val > node.next.val) {
+      [node.val, node.next.val] = [node.next.val, node.val];
+    }
+    return node;
+  }
+
+  const mid = findMidNode(node);
+  const left = node;
+  const right = mid.next;
+  mid.next = null;
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  let lnode = sortedLeft;
+  let rnode = sortedRight;
+
+  return mergeTwoOrderedList(lnode, rnode);
+};
 
 function sortList(head: ListNode | null): ListNode | null {
   if (!head) {
@@ -9,18 +70,8 @@ function sortList(head: ListNode | null): ListNode | null {
     return head;
   }
 
-  let node1 = head;
-
-  while (node1) {
-    let node2 = node1;
-    while (node2.next) {
-      if (node2.next.val < node2.val) {
-        [node2.next.val, node2.val] = [node2.val, node2.next.val];
-      }
-      node2 = node2.next;
-    }
-    node1 = node1.next;
-  }
-
-  return head;
+  return mergeSort(head);
 }
+
+const rt = sortList(testLinkedList);
+traverseLinkedList(rt);
