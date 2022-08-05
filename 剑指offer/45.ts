897e8625196ec51d0968a741e390fb1cb79e1605
Garfield-0927 <929780650@qq.com>
2022-08-05 22:17:29 +0800

    45

---
+++ 剑指offer/45.ts
@@ -0,0 +1,15 @@
+function minNumber(nums: number[]): string {
+  return nums
+    .map(String)
+    .sort((a, b) => {
+      if (a[0] !== b[0]) {
+        return a[0] > b[0] ? 1 : -1;
+      }
+
+      return a.concat(b) > b.concat(a) ? 1 : -1;
+    })
+    .join('');
+}
+
+console.log(minNumber([3,30,34,5,9]));
+
